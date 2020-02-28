9ee346067d371805f02f375c67ab20d7ed47f759
arloor <admin@arloor.com>
2020-02-28 19:57:12 +0800

    init

---
+++ src/main/java/Produceer.java
@@ -0,0 +1,54 @@
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.KafkaException;
+import org.apache.kafka.common.errors.AuthorizationException;
+import org.apache.kafka.common.errors.OutOfOrderSequenceException;
+import org.apache.kafka.common.errors.ProducerFencedException;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class Produceer {
+
+    private static final String TOPIC = "test";
+    private static final String BROKER_LIST = "arloor.com:9092";
+    private static KafkaProducer<String,String> producer = null;
+
+    /*
+    初始化生产者
+     */
+    static {
+        Properties configs = initConfig();
+        producer = new KafkaProducer<String, String>(configs);
+    }
+
+    /*
+    初始化配置
+     */
+    private static Properties initConfig(){
+        Properties properties = new Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,BROKER_LIST);
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
+        return properties;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        //消息实体
+        ProducerRecord<String , String> record = null;
+        for (int i = 0; i < 1000; i++) {
+            record = new ProducerRecord<String, String>(TOPIC, "value"+(int)(10*(Math.random())));
+            //发送消息
+            producer.send(record, new Callback() {
+                @Override
+                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                    if (null != e){
+                        System.out.println(e.getMessage());
+                    }else {
+                        System.out.println(String.format("offset:%s,partition:%s",recordMetadata.offset(),recordMetadata.partition()));
+                    }
+                }
+            });
+        }
+        producer.close();
+    }
+}
